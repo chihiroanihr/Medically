febc39e4b6328476996891586d3eea199c8c5c7c
alexandragafencu <47833199+alexandragafencu@users.noreply.github.com>
2019-03-03 06:18:55 -0500

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Medic.ly
+//
+//  Created by Alexandra Gafencu on 2019-03-02.
+//  Copyright © 2019 Alexandra Gafencu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
