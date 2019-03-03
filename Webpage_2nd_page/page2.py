bdf79bfd70f519070ebf9e66cdbd005746b6e3ca
chihiroanihr <chihiroanihr@gmail.com>
2019-03-03 12:36:52 -0500

    changes

--- Webpage_2nd_page/page2.py
+++ Webpage_2nd_page/page2.py
@@ -1,32 +1,7 @@
-from flask import Flask, render_template #追加
-import pymysql #追加
-
+from flask import Flask, render_template
 app = Flask(__name__)
-
 @app.route('/')
-def hello():
-
-    #db setting
-    db = pymysql.connect(
-            host='localhost',
-            user='root',
-            password='root',
-            db='testdb',
-            charset='utf8',
-            cursorclass=pymysql.cursors.DictCursor,
-        )
-
-    cur = db.cursor()
-    sql = "select * from members"
-    cur.execute(sql)
-    members = cur.fetchall()
-
-    cur.close()
-    db.close()
-
-    #return name
-    return render_template('hello.html', title='flask test', members=members) #変更
-
-## おまじない
-if __name__ == "__main__":
+def home():
+    return render_template('page2.html')
+if __name__ == '__main__':
     app.run(debug=True)
--- django_medicly/mysite/infos/forms.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm
-from .models import UserInput
-
-class UserInputForm(ModelForm):
-    class Meta:
-        model = UserInput
-        exclude = ()  # this says to include all fields from model to the form
-        #fields = ('customer', 'price', 'model')  # this would only display 3 fields on the form
